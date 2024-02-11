f5e77cb0c51d2bb6c5f6d475cd83cdbc82b65d46
tahsin469 <mughdaw825@gmail.com>
2024-02-12 00:23:20 +0600

    Add project files.

---
+++ DatabaseFirstFFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabaseFirstFFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DatabaseFirstFFCore/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirstFFCore.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; } = null!;
+        public string StudentGender { get; set; } = null!;
+        public int Age { get; set; }
+        public int Num { get; set; }
+    }
+}
